0edf200fee1f9d271bc6301888a0b5f8429b4d47
yihtserns <yihtserns@EAP180373286C42.apac.experian.local>
2013-11-05 22:40:02 +0800

    Revert "Allow route definition chaining in next line."
    This reverts commit d3107dc28789371ecf9988834d0c49025f14738b.

--- src/main/java/com/github/yihtserns/camelscript/CamelContextCategory.java
+++ src/main/java/com/github/yihtserns/camelscript/CamelContextCategory.java
@@ -18,6 +18,7 @@ package com.github.yihtserns.camelscript;
 import groovy.lang.Closure;
 import java.util.Arrays;
 import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.codehaus.groovy.runtime.GroovyCategorySupport;
 
@@ -51,7 +52,7 @@ public class CamelContextCategory {
      * @param self to add routes to
      * @param buildRoutePrototype things you'd do in {@link RouteBuilder#configure()}
      * @throws Exception if an error occurs while building route
-     * @see CamelContext#addRoutes(org.apache.camel.RoutesBuilder)
+     * @see CamelContext#addRoutes(RoutesBuilder)
      */
     public static void routes(final CamelContext self, final Closure buildRoutePrototype) throws Exception {
         self.addRoutes(new RouteBuilder() {
